1fd2dedbf11e4fd15a56f2c908e129520942eb5a
blackbox24 <nicardid@gmail.com>
2025-05-05 10:02:23 +0000

    release first version of app

---
+++ backend/urls.py
@@ -0,0 +1,32 @@
+from django.shortcuts import redirect
+from django.contrib import admin
+from django.urls import path,include
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+from allauth.account.middleware import AccountMiddleware
+
+app_name = "backend"
+
+schema = get_schema_view(
+    openapi.Info(
+        title="LHMS API BACKEND",
+        default_version="1.0.0",
+        description="backend api"
+    ),
+    permission_classes= (AllowAny,),
+)
+
+# REDIRECT INDEX PAGE TO DOCS
+def to_docs(request):
+    return redirect("swagger")
+
+urlpatterns = [
+    path("",to_docs,name="index"),
+    path("admin/", admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("swagger/docs/",schema.with_ui("swagger",cache_timeout=0),name="swagger"),
+]
